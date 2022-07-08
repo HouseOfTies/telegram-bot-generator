f28ca09d7cb2704d2c1360d516faf4ab69d044ad
seventty <elmastigeron2015@gmail.com>
2022-07-07 21:23:09 -0400

    ascii art in txt file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+#!/usr/bin/env node
+
+import * as fs from "fs";
+import * as path from "path";
+
+const text = fs.readFile(
+  path.join(__dirname, "./asciiArt.txt"),
+  "utf8",
+  (error, data) => {
+    console.log(typeof(data));
+  }
+);
