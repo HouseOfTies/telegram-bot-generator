c87c9c1b1b569c7a1fd7d68cec4fb089dba7b171
seventty <elmastigeron2015@gmail.com>
2022-07-07 22:21:18 -0400

    .env added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,26 @@ import * as fs from "fs";
 import * as path from "path";
 import asciiArt from "./asciiArt";
 
-const current_dir = process.cwd();
+const currentDir: string = process.cwd();
+const choices = fs.readdirSync(`${__dirname}/../cli`);
 
-console.log(current_dir);
+const questions = [
+    {
+      name: "telegram-bot-choice",
+      type: "list",
+      message: `What Telegram Bot template would yo like to generate? 🔥`,
+      choices: choices,
+    },
+    {
+      name: "telegram-bot-name",
+      type: "input",
+      message:
+        "How do you want to name your telegram bot? (Type <Bot> after the name, E.g: exampleBot) 🎀",
+      validate: function (input) {
+        if (/^([A-Za-z\-\_\d]+Bot)+$/.test(input)) return true;
+        else
+          return "Bot name may only include letters, numbers or underscores. Remember name your file with <Bot>, e.g: exampleBot";
+      },
+    },
+  ];
+
